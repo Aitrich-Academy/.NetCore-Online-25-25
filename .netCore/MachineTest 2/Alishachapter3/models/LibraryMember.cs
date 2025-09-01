da9cdbb1022de850efcd32c232370d9b8cd4819b
Alisha2727 <alishavarghese2019@gmail.com>
2025-09-01 14:51:03 +0530

    machine test

---
+++ .netCore/MachineTest 2/Alishachapter3/models/LibraryMember.cs
@@ -0,0 +1,14 @@
+﻿namespace Alishachapter3.models
+{
+    abstract class LibraryMember
+    {
+        public int MemberId { get; set; }
+        public string Name { get; set; }
+        public LibraryMember(int memberId, string name)
+        {
+            MemberId = memberId;
+            Name = name;
+        }
+        public abstract double CalculateFine(int overdueDays);
+    }
+}
---
+++ .netCore/MachineTest 2/Alishachapter3/models/members.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alishachapter3.models
+{
+    class StudentMember : LibraryMember
+    {
+        public StudentMember(int memberId, string name) : base(memberId, name) { }
+
+        private double FinePerDay = 1;
+        public override double CalculateFine(int overdueDays)
+        {
+            return overdueDays * FinePerDay;
+        }
+    }
+    class FacultyMember : LibraryMember
+    {
+        public FacultyMember(int memberId, string name) : base(memberId, name) { }
+
+        private double FinePerDay = 0.5;
+        public override double CalculateFine(int overdueDays)
+        {
+            return overdueDays * FinePerDay;
+        }
+    }
+}
