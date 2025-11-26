0efabfb5bb710f0294a9a3f8391a211ffeb5318a
Aneesha369 <aneesha.webdev@gmail.com>
2025-11-27 03:03:07 +0530

    project Added

---
+++ .netCore/Chapter 5/Aneesha/SQLQuery_1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE student;
+CREATE DATABASE  CompanyDB;
+CREATE TABLE Employees (
+    EmpID int, 
+    FirstName varchar(50),
+    LastName  varchar(50),
+    Salary decimal(10,2),
+    JoiningDate date
+)
+
